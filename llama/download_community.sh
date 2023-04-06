da2150fb6d7b642c1f6b323d709a27922e51e276
Bill Mill <bill@billmill.org>
2023-04-06 13:19:47 -0400

    rewrite download_community.bash

--- llama/download_community.sh
+++ llama/download_community.sh
@@ -1,50 +1,133 @@
-#!/bin/bash
-# UPDATE from Shawn (Mar 5 @ 2:43 AM)
+#!/usr/bin/env bash
+set -euo pipefail
+
+PRESIGNED_URL="https://agi.gpt4.org/llama/LLaMA"
+ALL_MODELS=7B,13B,30B,65B
+
+YELLOW=$(tput setaf 1)
+RED=$(tput setaf 3)
+CLEAR=$(tput sgr0)
+
+function usage {
+    cat <<"EOF"
+Usage: download_community [-vh] [<models>] [<output_directory>]
+
+Download the given llama <models> to <output_directory>. By default, will download 
+
+OPTIONS
+
+  -v, --verbose: enable verbose mode
+  -h, --help:    print this help and exit
+
+EXAMPLES
+
+Download all models ($ALL_MODELS) into the current directory
+
+  ./download_community.sh
+
+Download the 7B and 13B parameter models to /usr/share/llama
+    
+  ./download_community.sh 7B,13B /usr/share/llama
+
+EOF
+    exit 1
+}
+
+# print its argument in red and quit 
+function die {
+    printf "%s%s%s\n", "$RED", "$1", "$CLEAR"
+    exit 1
+}
+
+# download a file with a progress bar, then display a success message. Takes
+# two arguments: the URL and the output file name
+function download {
+    if ! wget --progress=bar:force "$1" -O "$2"; then
+        die "failed to download $1 -> $2"
+    fi
+    echo ✅ "$2"
+}
+
+# change into the model directory and use md5sum -c to verify the checksums of
+# the model files within. Uses a subshell to avoid changing the script's
+# direcotry
+function verify {
+    (cd "$1" && md5sum -c "$2")
+}
+
+# return the number of shards for a given model. Bash 3 doesn't support
+# associative arrays, so use a case statement instead.
+function nshards {
+    case $1 in
+        7B)
+            echo 0
+            ;;
+        13B)
+            echo 1
+            ;;
+        30B)
+            echo 3
+            ;;
+        65B)
+            echo 7
+            ;;
+        *)
+            die "invalid argument to nshards: $1"
+            ;;
+    esac
+
+}
+
+# parse the optional flags and discard them
+while true; do
+    case $1 in
+        -v|--verbose)
+            set -x
+            shift
+            ;;
+        -h|--help|help)
+            usage
+            ;;
+        *)
+            break
+            ;;
+    esac
+done
+
+# MODELS_TO_DOWNLOAD is a comma-separated list of models the user wants to
+# download, which defaults to all models. Split it into an array calld MODELS
+MODELS_TO_DOWNLOAD=${1:-$ALL_MODELS}
+IFS="," read -r -a MODELS <<< "$MODELS_TO_DOWNLOAD"
+
+# TARGET_FOLDER is the root directory to download the models to
+TARGET_FOLDER=${2:-.}
 
 echo "❤️ Resume download is supported. You can ctrl-c and rerun the program to resume the downloading"
 
-PRESIGNED_URL="https://agi.gpt4.org/llama/LLaMA/*"
+# ensure the targeted directory exists
+mkdir -p "$TARGET_FOLDER"
 
-MODEL_SIZE=${1:-7B,13B,30B,65B}  # edit this list with the model sizes you wish to download
-TARGET_FOLDER=${2:-./}           # where all files should end up
+printf "%sDownloading tokenizer...%s\n" "$YELLOW" "$CLEAR"
+download "$PRESIGNED_URL/tokenizer.model" "$TARGET_FOLDER/tokenizer.model"
+download "$PRESIGNED_URL/tokenizer_checklist.chk" "$TARGET_FOLDER/tokenizer_checklist.chk"
+verify "$TARGET_FOLDER" tokenizer_checklist.chk
 
-if [ $TARGET_FOLDER != "./" ]; then
-    mkdir -p $TARGET_FOLDER
-fi
-
-declare -A N_SHARD_DICT
-
-N_SHARD_DICT["7B"]="0"
-N_SHARD_DICT["13B"]="1"
-N_SHARD_DICT["30B"]="3"
-N_SHARD_DICT["65B"]="7"
-
-set -x
-echo "Downloading tokenizer..."
-wget --progress=bar:force ${PRESIGNED_URL/'*'/"tokenizer.model"} -O ${TARGET_FOLDER}"/tokenizer.model"
-echo ✅ ${TARGET_FOLDER}"/tokenizer.model"
-wget --progress=bar:force ${PRESIGNED_URL/'*'/"tokenizer_checklist.chk"} -O ${TARGET_FOLDER}"/tokenizer_checklist.chk"
-echo ✅ ${TARGET_FOLDER}"/tokenizer_checklist.chk"
-
-(cd ${TARGET_FOLDER} && md5sum -c tokenizer_checklist.chk)
-
-for i in ${MODEL_SIZE//,/ }
+# for each model, download each of its shards and then verify the checksums
+for model in "${MODELS[@]}"
 do
-    echo "Downloading ${i}"
-    mkdir -p ${TARGET_FOLDER}"/${i}"
-    for s in $(seq -f "0%g" 0 ${N_SHARD_DICT[$i]})
+    echo "Downloading $model"
+    mkdir -p "$TARGET_FOLDER/$model"
+
+    # download each shard in the model
+    for s in $(seq -f "0%g" 0 "$(nshards "$model")")
     do
-        #echo running: wget --continue --progress=bar:force ${PRESIGNED_URL/'*'/"${i}/consolidated.${s}.pth"} -O ${TARGET_FOLDER}"/${i}/consolidated.${s}.pth"
-        echo "downloading file to" ${TARGET_FOLDER}"/${i}/consolidated.${s}.pth" ...please wait for a few minutes ...
-        wget --continue --progress=bar:force ${PRESIGNED_URL/'*'/"${i}/consolidated.${s}.pth"} -O ${TARGET_FOLDER}"/${i}/consolidated.${s}.pth"
-        echo ✅ ${TARGET_FOLDER}"/${i}/consolidated.${s}.pth"
+        fout="$TARGET_FOLDER/$model/consolidated.$s.pth"
+        echo "downloading file to $fout ...please wait for a few minutes ..."
+        download "$PRESIGNED_URL/$model/consolidated.$s.pth" "$fout"
     done
-    wget --progress=bar:force ${PRESIGNED_URL/'*'/"${i}/params.json"} -O ${TARGET_FOLDER}"/${i}/params.json"
-    echo ✅ ${TARGET_FOLDER}"/${i}/params.json"
-    wget --progress=bar:force ${PRESIGNED_URL/'*'/"${i}/checklist.chk"} -O ${TARGET_FOLDER}"/${i}/checklist.chk"
-    echo ✅ ${TARGET_FOLDER}"/${i}/checklist.chk"
-    echo "Checking checksums"
-    (cd ${TARGET_FOLDER}"/${i}" && md5sum -c checklist.chk)
+    download "$PRESIGNED_URL/params.json" "$TARGET_FOLDER/$model/params.json"
+    download "$PRESIGNED_URL/checklist.chk" "$TARGET_FOLDER/$model/checklist.chk"
+
+    printf "%sChecking checksums%s" "$YELLOW" "$CLEAR"
+    verify "$TARGET_FOLDER/$model" checklist.chk
 done
-
-
